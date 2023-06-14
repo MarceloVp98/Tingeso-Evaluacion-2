446cd77d8063e80dce020573ad607bcda89487e6
Marclo Vargas Pavez <marcelo.vargas.p@usach.cl>
2023-06-14 03:49:48 -0400

    Cambios carpeta confi-data

--- acopio-service/src/main/java/com/tingesoevaluacion2/acopioservice/controllers/AcopioLecheController.java
+++ acopio-service/src/main/java/com/tingesoevaluacion2/acopioservice/controllers/AcopioLecheController.java
@@ -3,25 +3,27 @@ package com.tingesoevaluacion2.acopioservice.controllers;
 import com.tingesoevaluacion2.acopioservice.entities.AcopioLecheEntity;
 import com.tingesoevaluacion2.acopioservice.services.AcopioLecheService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
 
-@Controller
+@RestController
+@RequestMapping("/acopios")
 public class AcopioLecheController {
 
     @Autowired
     AcopioLecheService acopioLecheService;
 
-    @GetMapping("/subirArchivoAcopio")
-    public String subirArchivoAcopios(){
-        return "subirArchivoAcopio";
+    @GetMapping
+    public ResponseEntity<ArrayList<AcopioLecheEntity>> listasAcopios(){
+        ArrayList<AcopioLecheEntity> acopios= acopioLecheService.obtenerAcopiosLeche();
+        if(acopios.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(acopios);
     }
 
     @PostMapping("/subirArchivoAcopio")
@@ -30,12 +32,5 @@ public class AcopioLecheController {
         redirectAttributes.addFlashAttribute("mensaje", "Archivo cargado!");
         acopioLecheService.leerCSV(file.getOriginalFilename());
         return "redirect:/subirArchivoAcopio";
-    }
-
-    @GetMapping("/informacionAcopios")
-    public String listasAcopios(Model model){
-        ArrayList<AcopioLecheEntity> acopios= acopioLecheService.obtenerAcopiosLeche();
-        model.addAttribute("acopios", acopios);
-        return "informacionAcopios";
     }
 }
--- pago-service/src/main/java/com/tingesoevaluacion2/pagoservice/config/RestTemplateConfig.java
+++ pago-service/src/main/java/com/tingesoevaluacion2/pagoservice/config/RestTemplateConfig.java
@@ -1,4 +1,16 @@
 package com.tingesoevaluacion2.pagoservice.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
 public class RestTemplateConfig {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
